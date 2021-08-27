246d1a2baecd3c009b3dab52569b036b33fedd53
cubone <a>
2021-08-27 17:50:49 +0800

    feat: data into db

--- sdgp.py
+++ sdgp.py
@@ -1,9 +1,7 @@
 import time
-
 import requests
-
 from bs4 import BeautifulSoup
-import csv
+import pymysql
 
 
 def request_data(url):
@@ -19,9 +17,6 @@ def request_data(url):
         return None
 
 
-# 定义存储变量
-data = []
-
 domain = "http://www.ccgp-shandong.gov.cn"
 
 
@@ -33,13 +28,12 @@ def parse_announcement(html):
     href_list = soup.select('a[href^="/sdgp2017/site/listcontnew.jsp?colcode=0302&"]')
     urls = []
     for href in href_list:
-        # print(domain + href.get('href'))
         urls.append(domain + href.get('href'))
     return urls
 
 
-def parse_result(html, href):
-    cache_data = ['', '', '', '', '', '', '', '']
+def parse_result(html):
+    cache_data = ['', '', '', '', '', '', '']
     if html is None:
         return
     soup = BeautifulSoup(html, 'lxml')
@@ -51,29 +45,19 @@ def parse_result(html, href):
     td_list = table_list[0].find_all("td")
     for item in td_list:
         if "采购项目名称" in item.text:
-            item_name = item.text.strip().split("：")[1]
-            cache_data[1] = item_name
-            # print(item_name)
+            cache_data[1] = item.text.strip().split("：")[1]
 
         if "采购项目编号" in item.text:
-            item_sn = item.text.strip().split("：")[1]
-            cache_data[2] = item_sn
-            # print(item_sn)
+            cache_data[2] = item.text.strip().split("：")[1]
 
         if "采购人" in item.text:
-            purchaser = item.text.strip().split("：")[1].split()[0].split(",")[0]
-            cache_data[3] = purchaser
-            # print(purchaser)
+            cache_data[3] = item.text.strip().split("：")[1].split()[0].split(",")[0]
 
         if "成交日期" in item.text:
-            transaction_date = item.text.strip().split("：")[1]
-            cache_data[4] = transaction_date
-            # print(transaction_date)
+            cache_data[4] = item.text.strip().split("：")[1]
 
         if "代理机构" in item.text:
-            agency = item.text.strip().split("：")[1].split()[0]
-            cache_data[5] = agency
-            # print(agency)
+            cache_data[5] = item.text.strip().split("：")[1].split()[0]
 
     detail_str = "货物服务名称：%s，供应商名称：%s"
     detail = ""
@@ -81,26 +65,11 @@ def parse_result(html, href):
     for index in range(1, len(tr_list)):
         td_list = tr_list[index].find_all("td")
         detail += detail_str % (td_list[1].text.strip(), td_list[2].text.strip()) + "\n"
-        # print(detail)
     cache_data[6] = detail
-    cache_data[7] = href
     return cache_data
 
 
-def write_data(data_):
-    print('开始写入数据 ====> ')
-    # def create_csv():
-    title_data = ['公告名称', '采购项目名称', '采购项目编号', '采购人', '成交日期', '代理机构', '采购详情', "链接地址"]
-    with open('省结果公告101-201.csv', 'w', newline='', encoding='utf-8-sig') as csv_file:
-        writer = csv.writer(csv_file)
-        writer.writerow(title_data)
-        for info in data_:
-            writer.writerow(info)
-
-    csv_file.close()
-
-
-def main(page):
+def add_new(page, db):
     # 省结果公告地址
     url = 'http://www.ccgp-shandong.gov.cn/sdgp2017/site/listnew.jsp?grade=province&colcode=0302&curpage=' + str(
         page) + '&grade=province&firstpage=1'
@@ -114,12 +83,37 @@ def main(page):
         # 获取公告内容
         data_html = request_data(href)
         # 解析内容
-        data.append(parse_result(data_html, href))
+        data = parse_result(data_html)
+
+        # SQL 插入语句
+        sql = """INSERT INTO ccgp(announcement_name,prj_name, prj_sn, purchaser, deal_date,agency,detail,href)
+                     VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"""
+        param = (data[0], data[1], data[2], data[3], data[4], data[5], data[6], href)
+        print(param)
+        try:
+            with db.cursor() as cursor:
+                if cursor.execute(sql, param) == 1:
+                    print('insert successful')
+        except pymysql.MySQLError as error:
+            print('insert fail')
+            print(error)
+            # 如果发生错误则回滚
+            db.rollback()
+
+
+def main():
+    # 打开数据库连接
+    db = pymysql.connect(host='146.56.178.192', port=6033,
+                         user='root', passwd='svend123',
+                         db='ccgp', charset='utf8mb4',
+                         autocommit=True,
+                         cursorclass=pymysql.cursors.DictCursor)
+    for i in range(11, 31):
+        add_new(i, db)
+        time.sleep(1)
+    # 关闭数据库连接
+    db.close()
 
 
 if __name__ == "__main__":
-    for i in range(101, 201):
-        main(i)
-        time.sleep(1)
-
-    write_data(data)
+    main()
